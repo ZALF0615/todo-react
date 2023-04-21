8bf1b78ca97c348c5e8abf5ae8cb0356358d2fa6
ZALF <tabu.tabu.daze@gmail.com>
2023-04-21 15:29:29 +0900

    add upper lower features

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,7 +8,7 @@ import React from "react";
 import styles from "@/styles/TodoList.module.css";
 
 // TodoItem 컴포넌트를 정의합니다.
-const TodoItem = ({ todo, onToggle, onDelete }) => {
+const TodoItem = ({ todo, onToggle, onDelete, onUpper, onLower}) => {
   // 각 할 일 항목을 렌더링합니다.
   return (
     <li className={styles.todoItem}>
@@ -25,7 +25,9 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
       </span>
 
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
-      <button onClick={onDelete}>Delete</button>
+      <button onClick={onUpper}>🔺</button>
+      <button onClick={onLower}>🔻</button>
+      <button onClick={onDelete}>❌</button>
     </li>
   );
 };
